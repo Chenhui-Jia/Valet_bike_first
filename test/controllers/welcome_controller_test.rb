03b26822cd2438b969b51e12357e02b0cbd79911
Chenhui-Jia <65981990+Chenhui-Jia@users.noreply.github.com>
2022-03-29 10:02:33 -0400

    user signUp, signIn, and signOut
    Add models, controllers, and views for 3 action; delete the sidebars(need to add back when other pages are finished)

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
-class UserTest < ActiveSupport::TestCase
-  
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end

86562653ed9f9058599260a42af8937a1e87e740
Chenhui-Jia <65981990+Chenhui-Jia@users.noreply.github.com>
2022-04-04 21:03:12 -0400

    Add reset password and forget password session
    1. The reset password works fine
    2. The forget password should send out an email to reset the password but no email was sent out right now [could delete this function later]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,16 +3,27 @@ class RegistrationsController < ApplicationController
    def new
      @user = Users.new
    end
+   # def create
+   #   @user = Users.new(user_params)
+   #   if @user.save
+   #   # stores saved user id in a session
+   #     session[:user_id] = @user.id
+   #     redirect_to root_path, notice: 'Successfully created account'
+   #   else
+   #     render :new
+   #   end
+   # end
    def create
-     @user = Users.new(user_params)
-     if @user.save
-     # stores saved user id in a session
-       session[:user_id] = @user.id
-       redirect_to root_path, notice: 'Successfully created account'
-     else
-       render :new
-     end
-   end
+  @user = Users.new(user_params)
+  if @user.save
+    WelcomeMailer.with(user: @user).welcome_email.deliver_now
+    # deliver_now is provided by ActiveJob.
+    session[:user_id] = @user.id
+    redirect_to root_path, notice: 'Successfully created account'
+  else
+    render :new
+  end
+end
    private
    def user_params
      # strong parameters

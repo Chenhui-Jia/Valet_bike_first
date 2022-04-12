c393a784a39cad3ee0a7e6ba6f342668a344b40e
kalllie <98468732+kalllie@users.noreply.github.com>
2022-04-12 15:56:08 -0400

    add station model and rake task for importing data, add station markers to map

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    # for displaying station markers on map
+    @markers = Station.all.collect do |station|
+      {:latlng => [station.latitude, station.longitude],
+       :popup => "<b>#{station.name}</b><br>#{station.address}<br>(x) docked bikes<br>(x) empty docks"}
+    end
   end
 end

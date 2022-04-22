c9574cbb550d521a1eba92ea63ae68eafd64e625
kallie <98468732+kalllie@users.noreply.github.com>
2022-04-22 14:10:43 -0400

    add bike model and establish relationships between bike and station models. show number of docked bikes and empty docks on station map.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_12_152222) do
+ActiveRecord::Schema.define(version: 2022_04_22_180014) do
+
+  create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
+    t.integer "identifier"
+    t.integer "current_station_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
     t.integer "identifier"
@@ -26,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_04_12_152222) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
+    t.string "payment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

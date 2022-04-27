4d590af95d2fab51af1633d37211565746997ecb
kallie <98468732+kalllie@users.noreply.github.com>
2022-04-27 09:48:50 -0400

    remove a docked bike from the station when user starts ride from map page

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
     def show
         @markers = Station.all.collect do |station|
             {:latlng => [station.latitude, station.longitude],
-             :popup => "<b>#{station.name}</b><br>#{station.address}<br>#{station.docked_bikes.count} docked bikes<br>#{station.dock_count - station.docked_bikes.count} empty docks <br><a href='start'>start</a> "}
+             :popup => "<b>#{station.name}</b><br>#{station.address}<br>#{station.docked_bikes.count} docked bikes<br>#{station.dock_count - station.docked_bikes.count} empty docks <br><a href='start/#{station.identifier}'>start</a> "}
         end
     end
 end
--- app/controllers/ride_controller.rb
+++ app/controllers/ride_controller.rb
@@ -3,8 +3,12 @@ class RideController < ApplicationController
     end
 
     def start
-  
+      if params[:station_id]
+        #remove a bike from the station passed as a param
+        bike = Bike.find_by current_station_id: params[:station_id]
+        bike ? bike.update(current_station_id: null) : puts "no bike found" #find another way to deal with no bike found case
     end
+
     def end
     end
 

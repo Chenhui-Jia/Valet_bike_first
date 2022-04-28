d3945ef749f46927fa379d53de00cfb745e04307
kallie <98468732+kalllie@users.noreply.github.com>
2022-04-28 11:05:53 -0400

    remove map code from welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    # for displaying station markers on map
-    @markers = Station.all.collect do |station|
-      {:latlng => [station.latitude, station.longitude],
-       :popup => "<b>#{station.name}</b><br>#{station.address}<br>#{station.docked_bikes.count} docked bikes<br>#{station.dock_count - station.docked_bikes.count} empty docks<br><a href='start'>start</a>"
-     }
-
-
-
-    end
   end
 
   def profile

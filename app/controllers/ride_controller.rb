aa8192758eccd3fb694a872a4394f75e8a5075dc
kallie <98468732+kalllie@users.noreply.github.com>
2022-04-28 11:04:39 -0400

    fix errors with last commit

--- app/controllers/ride_controller.rb
+++ app/controllers/ride_controller.rb
@@ -3,10 +3,17 @@ class RideController < ApplicationController
     end
 
     def start
-      if params[:station_id]
-        #remove a bike from the station passed as a param
-        bike = Bike.find_by current_station_id: params[:station_id]
-        bike ? bike.update(current_station_id: null) : puts "no bike found" #find another way to deal with no bike found case
+      # todo: make sure there is a station passed in the params hash (not null) - 
+      # remove the route for start without station id and verify here
+      @bike = Bike.find_by(current_station_id: params[:station_id])
+      # make sure there is a bike docked at the station
+      redirect_to "/map" if !@bike # todo: add some kind of "no docked bikes" message
+
+      if @bike.update(current_station_id: nil)
+        # todo: redirect_to different page to avoid checking out another bike if the user refreshes the page
+      else
+        # todo: idk show an error message or something
+      end
     end
 
     def end
